2c71eed3d43e66acca95bd469d9dc877acc5f212
Subramani B <94166127+Subramaniya-pillai@users.noreply.github.com>
2025-03-21 12:42:24 +0530

    task1.py

--- program-TASK1/task1.py
+++ program-TASK1/task1.py
@@ -12,4 +12,3 @@ def check_order_status(status):
 status = input("Enter order status (Processing, Delivered, Cancelled): ")
 check_order_status(status)
 
-https://github.com/Subramaniya-pillai/courier_management_system/blob/2e321419144b55c4cca05937c1e0e73fdaccd3f8/program-TASK1/image.png
